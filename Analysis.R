3ebc105bb113ed0f47736b08c8d88120526f9b01
Frank D'Agostino <frankdag20@gmail.com>
2020-06-18 00:00:51 -0400

    Add files via upload

--- Analysis.R
+++ Analysis.R
@@ -4,13 +4,13 @@
 
 rm(list = ls())
 # Clear console
-library(ggplot2)
-library(plyr)
-library(sandwich)
-library(lmtest)
-library(ggpubr)
-library(tidyverse)
-library(broom)
+library(ggplot2); library(plyr)
+library(foreign); library(dplyr)
+library(MASS); library(lme4)
+library(glmmTMB); library(gamm4)
+library(sandwich); library(lmtest)
+library(rdrobust); library(ggpubr)
+library(tidyverse); library(broom)
 library(AICcmodavg)
 
 #-------------------------------------------------------------------------------
@@ -24,7 +24,7 @@ hosp <- read.csv("countyHosp.csv"); head(hosp)
 
 # For air pollution, we have data spanning back to 2010, but we want
 # to focus on data regarding 2019 and 2020, so we take a subset
-recent <- subset(air, Year >= 2016 & Pollutant == "PM2.5"); head(recent)
+recent <- subset(air, Year >= 2010 & Pollutant == "PM2.5"); head(recent)
 
 # Clean the data by creating a dataframe of average AQI values for
 # graphing and easy visualization
@@ -34,12 +34,12 @@ essex <- subset(recent, County == "Essex")
 franklin <- subset(recent, County == "Franklin"); hampden <- subset(recent, County == "Hampden")
 hamp <- subset(recent, County == "Hampshire"); middle <- subset(recent, County == "Middlesex")
 norf <- subset(recent, County == "Norfolk"); plym <- subset(recent, County == "Plymouth")
-suff <- subset(recent, County == "Suffolk"); worc <- subset(recent, County == "Worchester")
+suff <- subset(recent, County == "Suffolk"); worc <- subset(recent, County == "Worcester")
 
 # Middlesex and Norfolk have sparse data
 dataMean <- data.frame(Countys = c("Berkshire", "Bristol", # "Dukes", 
                                    "Essex", "Franklin", "Hampden", "Hampshire", "Middlesex",
-                                    "Norfolk", "Plymouth", "Suffolk", "Worchester"),
+                                    "Norfolk", "Plymouth", "Suffolk", "Worcester"),
                    AQI = c(mean(berk$AQI), mean(brist$AQI), # mean(dukes$AQI),
                            mean(essex$AQI), mean(franklin$AQI), mean(hampden$AQI),
                            mean(hamp$AQI), mean(middle$AQI), mean(norf$AQI),
@@ -48,6 +48,14 @@ dataMean <- data.frame(Countys = c("Berkshire", "Bristol", # "Dukes",
 #-------------------------------------------------------------------------------
 # Basic Visualization
 #-------------------------------------------------------------------------------
+
+# Plotting AQI over time
+ggplot(suff, aes(x=Year,y=AQI)) + stat_summary(fun.y = "mean",geom="point") +
+  stat_summary(fun.y = "mean",geom="line")  +
+  labs(x = "Year", y = "Air Quality Index", shape = "") +
+  ggtitle("Progression of Air Quality (PM2.5) over the Last Decade in Suffolk County, MA")
+  # + theme(legend.position="bottom") # + geom_vline(xintercept=2020)
+  # shape= factor(Pollutant, labels = c("Rest of U.S.", "Arizona")))) +
 
 # Use ggplot to visually demonstrate the relatively small differences
 # in AQI (stands for air quality index)
@@ -70,24 +78,34 @@ barplot(table(hosp$HospTotal, hosp$County),
 #-------------------------------------------------------------------------------
 
 # Using regex, we want to select air pollution data from only the months
-# of April and May from 2020, since the hospitalization data spans from
+# of January-May from 2020, since the hospitalization data spans from
 # 4/30/20 to 5/22/20, which is the most recent date
-count <- recent$County[grep("^([5][\\/]|[4][\\/])\\d+(\\/20)", recent$Date)]; head(count)
-poll <- recent$AQI[grep("^([5][\\/]|[4][\\/])\\d+(\\/20)", recent$Date)]; head(poll)
+count <- recent$County[grep("^([5][\\/]|[4][\\/]|[3][\\/]|[2][\\/]|[1][\\/]|)\\d+(\\/20)", recent$Date)]; head(count)
+poll <- recent$AQI[grep("^([5][\\/]|[4][\\/]|[3][\\/]|[2][\\/]|[1][\\/]|)\\d+(\\/20)", recent$Date)]; head(poll)
 mayAir <- data.frame(count, poll); head(mayAir)
 
 # Use the aggregate function to group everything by counties
 temp <- aggregate(x=hosp, by=list(hosp$County), FUN=mean); temp
-aggHosp <- subset(temp, Group.1 != "Barnstable" & Group.1 != "Nantucket" & Group.1 != "Dukes"); aggHosp
+aggHosp <- subset(temp, Group.1 != "Nantucket" & Group.1 != "Barnstable" & Group.1 != "Dukes"); aggHosp
 
-allAir <- data.frame(recent$County, recent$AQI); head(allAir)
-aggAir <- aggregate(x=allAir, by=list(allAir$recent.County), FUN=mean); aggAir
+allAir <- data.frame(dataMean$County, dataMean$AQI); head(allAir)
+aggAir <- aggregate(x=allAir, by=list(allAir$dataMean.County), FUN=mean); aggAir
 
-# Nantucket and Barnstable do not have enough Air data in April/May 2020,
+# Nantucket, Dukes, and Barnstable do not have enough Air data in April/May 2020,
 # and so we remove them from the hospital data to have even comparisons
+
+# We also add the confounding variables to them
+c1 <- read.csv("confoundVars.csv"); c1
+c2 <- read.csv("confoundPercent.csv"); c2
+conf1 <- subset(c1, County != "Nantucket" & County != "Barnstable" & County != "Dukes"); conf1
+conf2 <- subset(c2, County != "Nantucket" & County != "Barnstable" & County != "Dukes"); conf2
+
 dataComb <- data.frame(County = aggAir$Group.1,
-                       AQI = aggAir$recent.AQI, HospTotal = aggHosp$HospTotal,
-                       ICU = aggHosp$ICU); dataComb
+                       AQI = aggAir$dataMean.AQI, HospTotal = aggHosp$HospTotal,
+                       ICU = aggHosp$ICU, Pop = conf1$Total.Pop,
+                       Young = conf1$Under.18, Old = conf1$X65...Over,
+                       Cancer = conf1$Lung.Cancer, Nonwhite = conf1$Non.White,
+                       Poverty = conf1$Poverty.Estimate); dataComb
 
 #-------------------------------------------------------------------------------
 # Check for Preliminary Relationships
@@ -110,6 +128,19 @@ curve(-0.7115*x + 36.3471,col = "cyan", lwd=3, add = T)
 # Additionally, more ICU hospitalizations are associated
 # with a decreased AQI
 coeftest(icu.lm, vcov=vcovHC(icu.lm,type="HC1"))
+
+# Let's look at nonwhite population
+nwAvg <- mean(dataComb$Nonwhite); nwAvg
+highNW <- subset(dataComb, Pop > nwAvg); highNW
+lowNW <- subset(dataComb, Pop <= nwAvg); lowNW
+t.test(highNW$HospTotal, lowNW$HospTotal, equal.values = FALSE)
+# Significant value of 0.004807 - extremely interesting!
+
+(ggplot(dataComb, aes(x = HospTotal, y = AQI)) + stat_summary_bin(fun.y='mean', bins=25, color='deeppink4', size=1, alpha=0.7, geom='point', na.rm = TRUE)+labs(y= "Average AQI", x = "COVID-19 Hospitalizations")+ggtitle("Binned Scatter Plot of Average AQI vs. COVID Hospitalizations per County"))
+(ggplot(dataComb, aes(x = HospTotal, y = Pop)) + stat_summary_bin(fun.y='mean', bins=25, color='deeppink4', size=1, alpha=0.7, geom='point', na.rm = TRUE)+labs(y= "Population", x = "COVID-19 Hospitalizations")+ggtitle("Binned Scatter Plot of Population vs. COVID Hospitalizations per County"))
+(ggplot(dataComb, aes(x = HospTotal, y = Nonwhite)) + stat_summary_bin(fun.y='mean', bins=25, color='deeppink4', size=1, alpha=0.7, geom='point', na.rm = TRUE)+labs(y= "Nonwhite Population", x = "COVID-19 Hospitalizations")+ggtitle("Binned Scatter Plot of Nonwhite Population vs. COVID Hospitalizations per County"))
+(ggplot(dataComb, aes(x = HospTotal, y = Poverty)) + stat_summary_bin(fun.y='mean', bins=25, color='deeppink4', size=1, alpha=0.7, geom='point', na.rm = TRUE)+labs(y= "Population Living in Poverty", x = "COVID-19 Hospitalizations")+ggtitle("Binned Scatter Plot of Population in Poverty vs. COVID Hospitalizations per County"))
+(ggplot(dataComb, aes(x = HospTotal, y = Old)) + stat_summary_bin(fun.y='mean', bins=25, color='deeppink4', size=1, alpha=0.7, geom='point', na.rm = TRUE)+labs(y= "Population Aged 65+", x = "COVID-19 Hospitalizations")+ggtitle("Binned Scatter Plot of Older Population vs. COVID Hospitalizations per County"))
 
 #-------------------------------------------------------------------------------
 # Perform a Permutation Test with Air Quality Data from 2016 to 2020
@@ -290,20 +321,5 @@ t.test(above$ICU, below$ICU, equal.values = FALSE)
 # The built-in t-test for unequal variances gives p-value = 0.6949, 
 # which means that our classical method agrees with our simulation method 
 # of a permutation test.
-
-#-------------------------------------------------------------------------------
-# Perform a Permutation Test with ICU Data
-#-------------------------------------------------------------------------------
-
-# Check for normality
-hist(dataComb$AQI)
-# Seems somewhat normal
-
-anovaTest = aov(berk$AQI~brist$AQI+dukes$AQI+
-                essex$AQI+franklin$AQI+hampden$AQI+
-                hamp$AQI+middle$AQI+norf$AQI+
-                plym$AQI+suff$AQI+worc$AQI) # Runs ANOVA test
-ls(anovaTest) # Lists items stored by the test
-summary(anovaTest) # Gives basic ANOVA output
 
 ##################################################################
